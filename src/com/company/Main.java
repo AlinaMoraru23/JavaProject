51ca45ca7369b018ee7639698fded245ccb95130
Alina Moraru <alinamoraru@CornelscbookPro.localdomain>
2019-05-23 20:39:53 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello,");
+
+        byte myDay = 23 ;
+        String myMonth = "04" ;
+        short myYear = 1990 ;
+        System.out.println();
+        System.out.println("I was born in: " + myDay + "." + myMonth +"."+ myYear);
+
+    }
+}
